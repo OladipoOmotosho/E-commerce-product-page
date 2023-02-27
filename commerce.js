35dbed75c8cceb9b50301e76d99c98f9790be9af
Oladipo <is.haqomotosho@gmail.com>
2023-02-27 10:45:13 +0100

    new changes

--- commerce.js
+++ commerce.js
@@ -1,3 +1,4 @@
+// This code creates variables and assigns DOM elements to them
 const closeBtn = document.getElementById("closeOrange");
 const modal = document.getElementById("modalBg");
 const displayItems = document.querySelectorAll("#overlay");
@@ -11,27 +12,31 @@ const thumbnails = document.querySelectorAll("#thumbnailss");
 const overlay = document.getElementsByClassName("overlayy");
 const preview = document.querySelectorAll(".highlight");
 
+// These variables keep track of the current state of the image carousel
 var currentIndex = 1;
 var currentImg = 1;
 
+// These functions are event handlers that respond to user interactions
 closeBtn.addEventListener("click", closeModal);
 prevBtn.addEventListener("click", showPrevImage);
 nextBtn.addEventListener("click", showNextImage);
 displayItems.forEach((item) => item.addEventListener("click", openModal));
 cart.addEventListener("click", toggleCart);
 
+// This function hides the modal when called
 function closeModal() {
   modal.style.display = "none";
 }
 
+// This function shows the modal when called
 function openModal() {
   for (let i = 0; i < displayItems.length; i++) {
     modal.style.display = "block";
   }
 }
 
+// This function shows or hides the cart dropdown when called
 function toggleCart() {
-  console.log(displayItems);
   if (displayCart.style.display === "none") {
     displayCart.style.display = "block";
   } else {
@@ -39,6 +44,7 @@ function toggleCart() {
   }
 }
 
+// This function displays the previous image in the carousel
 function showPrevImage() {
   if (currentIndex === 1) {
     currentIndex = 4;
@@ -48,6 +54,7 @@ function showPrevImage() {
   mainImgPop.src = `./images/product${currentIndex}.jpg`;
 }
 
+// This function displays the next image in the carousel
 function showNextImage() {
   if (currentIndex == 4) {
     currentIndex = 1;
@@ -57,13 +64,7 @@ function showNextImage() {
   mainImgPop.src = `./images/product${currentIndex}.jpg`;
 }
 
-thumbnails.forEach((thumbnail) => {
-  thumbnail.addEventListener("click", () => {
-    // Code to open the pop modal here
-    console.log("Pop modal opened");
-  });
-});
-
+// This code adds click event listeners to each preview image
 preview.forEach((highlight) => {
   highlight.addEventListener("click", () => {
     // Get the ID of the clicked highlight
